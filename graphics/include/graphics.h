4daee677d542fd09a0aab3a36065e4f48145e14d
Victoria Tisdale <vhtisdale@gmail.com>
2014-10-13 17:37:10 -0400

    added graphics.h to make test files

---
+++ graphics/include/graphics.h
@@ -0,0 +1,13 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#include "ppmIO.h"
+#include "image.h"
+#include "objects.h"
+#include "list.h"
+#include "fractals.h"
+#include "scanline.h"
+#include "polygon.h"
+#include "matrix.h"
+
+#endif
