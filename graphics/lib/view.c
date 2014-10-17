143c1597562e8364d19ea1474908ed359eb4bb80
Christopher J. Burnham <cburnham@fili.cs.colby.edu>
2014-10-17 13:02:04 -0400

    print statements in 3D matrix to debug cube

--- graphics/lib/view.c
+++ graphics/lib/view.c
@@ -21,6 +21,8 @@ void matrix_setView3D(Matrix *vtm, View3D *view){
 
 	matrix_identity(vtm);
 	matrix_translate(vtm, -view->vrp.val[0], -view->vrp.val[1], -view->vrp.val[2]);
+	printf("After VRP\n");	
+	matrix_print(vtm, stdout);
 
 	vpn = view->vpn;
 	vector_cross(&view->vup, &vpn, &u);
@@ -30,16 +32,24 @@ void matrix_setView3D(Matrix *vtm, View3D *view){
 	vector_normalize(&vpn);
 
 	matrix_rotateXYZ(vtm, &u, &vup, &vpn);
+	printf("After rotateXYZ\n");
+	matrix_print(vtm, stdout);
 	matrix_translate(vtm, 0.0, 0.0, view->d);
+	printf("After COP\n");
+	matrix_print(vtm, stdout);
 	
 	// in lecture notes here (6 and 7) it says to shear but as we only have d to define the COP I don't think we have to
 
 	b = view->d + view->b;
 	
 	matrix_scale(vtm, ((2.0*view->d) / (b*view->du)), ((2.0*view->d) / (b*view->dv)), (1/b));
+	printf("After scale CVV\n");
+	matrix_print(vtm, stdout);
 	
 	matrix_perspective(vtm, view->d);
+	printf("After perspective\n");
 	matrix_scale2D(vtm, (-view->screenx / (2.0*view->d)), (-view->screeny / (2.0*view->d)));
 	matrix_translate2D(vtm, (view->screenx / 2.0), (view->screenx / 2.0));
-	
+	printf("final\n");
+	matrix_print(vtm, stdout);
 }
